f3d0d01d51fde2e1d0bfa7eecec72c7e1d36d8b0
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-03-23 16:28:12 +0700

    Commit

--- admin/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="pull-right">
-		<footer>
-           <p>Programmed by: John Kevin Lorayna BSIS 4-A</p>
-        <footer>
-</div>
--- footer.php
+++
@@ -1,8 +0,0 @@
-<center>
-		<footer>
-		
-		<p>CHMSC M-Learning Copyright 2013</p>
-			<!-- <p>Programmed by: John Kevin Lorayna BSIS 4-A</p> -->
-		</footer>
-</center>
-
